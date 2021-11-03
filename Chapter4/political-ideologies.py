2056eb2b155372353b4c473464323702b38fc9a4
Saewoo Gimbap <36112434+Jjajang-myeon@users.noreply.github.com>
2021-11-03 15:00:57 -0500

    finish chapter4

---
+++ Chapter4/political-ideologies.py
@@ -0,0 +1,84 @@
+from pylab import *
+
+"""
+implement dynamics of three political ideologies
+"""
+class IdeologiesModel(object):
+    """
+    c, l, n : proportions of conservative, liberal, and neutral
+        0 <= c + l + n <= 1, each proportions are between 0 and 1, inclusive
+    r : constant for rate
+        r > 0
+    result : time series of state variable
+    timeSteps : list of time
+    """
+    def __init__(self, r, c0, n0, l0):
+        self.r = r
+        self.c = c0
+        self.n = n0
+        self.l = l0
+        self.cresult = [self.c]
+        self.nresult = [self.n]
+        self.lresult = [self.l]
+        self.t = 0
+        self.timeSteps = [self.t]
+    
+    # store state variables in list
+    def observe(self):
+        self.cresult.append(self.c)
+        self.nresult.append(self.n)
+        self.lresult.append(self.l)
+        self.timeSteps.append(self.t)
+    
+    # update state variable according to difference equation
+    def update(self):
+        consvRate = 1
+        libRate = 1
+        neutRate = 1
+
+        # find which side is popular, neutral or conservative, then adjust rate
+        deltaRate = self.r * (self.n - self.c)
+        neutRate += deltaRate
+        consvRate -= deltaRate
+        
+        # find which side is popular, liberal or neutral, then adjust rate
+        deltaRate = self.r * (self.l - self.n)
+        libRate += deltaRate
+        neutRate -= deltaRate
+
+        # find which side is popular, conservative or liberal, then adjust rate
+        deltaRate = self.r * (self.c - self.l)
+        consvRate += deltaRate
+        libRate -= deltaRate
+        
+        # update actual proportions
+        self.c *= consvRate
+        self.l *= libRate
+        self.n *= neutRate
+        self.t += 1
+
+        # normalize proportions so that they sum up to 1
+        normalizingFactor = 1 / (self.c + self.n + self.l)
+        self.c *= normalizingFactor
+        self.n *= normalizingFactor 
+        self.l *= normalizingFactor
+
+if __name__ == "__main__":
+    r = 0.1
+    c0 = 0.33
+    n0 = 0.34
+    l0 = 0.33
+    myModel = IdeologiesModel(r, c0, n0, l0)
+    timeSpan = 30
+    while myModel.t < timeSpan:
+        myModel.update()
+        myModel.observe()
+    xlabel("Time")
+    ylabel("Proportion")
+    plot(myModel.timeSteps, myModel.cresult, 'r-')
+    plot(myModel.timeSteps, myModel.nresult, '+')
+    plot(myModel.timeSteps, myModel.lresult, 'b--')
+    
+    # title("Phase Space")
+    # plot(myModel.xresult, myModel.yresult)
+    show()
